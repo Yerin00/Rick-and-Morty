63da6644bdd1c293d04962b346022b865064905e
Yerin <zlekfl0311@gmail.com>
2022-10-18 21:28:27 +0900

    rickandmorty complete code

---
+++ src/components/Characters.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import Character from './Character'
+
+export default function Characters() {
+    const [page, setPage] = useState(1)
+    
+    const fetchCharacters = async ({ queryKey }) => {
+        const response = await fetch(
+            `https://rickandmortyapi.com/api/character?page=${queryKey[1]}`
+        )
+        return response.json()
+    }
+
+    const { data, status, isPreviousData } = useQuery(["characters", page], fetchCharacters, {
+        keepPreviousData: true,
+    })
+    
+    if (status === "loading"){ 
+        return <div>Loading...</div>
+    }
+    if (status === "error"){
+        return <div>Error</div>
+    }
+    
+
+    return (
+        <div className="characters">
+            {data.results.map((character) => (
+                <Character character={character} />
+            ))}
+            <div>
+                <button disabled={page === 1} onClick={() => setPage(page - 1)}>
+                    Previous
+                </button>
+                <button disabled={isPreviousData || !data.info.next} onClick={() => setPage(page + 1)}>
+                    Next
+                </button>
+            </div>
+        </div>
+    )
+}
